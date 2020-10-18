55dcde3647dc7a04a166e3e2a3361be29ab9af70
Teek Leng Ng <jeff2406n@gmail.com>
2020-10-18 22:57:16 +0100

    Movie Details Page - included production countries

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -51,6 +51,16 @@ export default ({ movie }) => {
           </li>
         ))}
       </ul>
+      <ul className="list-group list-group-horizontal">
+        <li key="pcouh" className="list-group-item list-group-item-dark">
+          Production Countries
+        </li>
+          {movie.production_countries.map(pcou => (
+            <li key={pcou.name} className="list-group-item">
+                {pcou.name}
+            </li>
+          ))}
+    </ul>
     </>
   );
 };
