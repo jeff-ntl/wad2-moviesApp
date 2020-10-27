551ef86c1f7dbd88612e945a9147e5f4a3ae4435
Teek Leng Ng <jeff2406n@gmail.com>
2020-10-27 21:52:43 +0000

    Added routing and live data from API

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
 
-const MovieListPage = ({movies}) => {
+const MovieListPage = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=82daa40f5828cda045eb76fd2fcb0cdb&language=en-US&include_adult=false&page=1`    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        return json.results
+      })
+      .then(movies => {
+        setMovies(movies);
+      });
+  }, []);
   return (
     <>
       <Header numMovies={movies.length} />
