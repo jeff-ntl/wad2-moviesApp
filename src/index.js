4c5abfc2caed51353765343e89da10fab41312c3
Teek Leng Ng <jeff2406n@gmail.com>
2020-11-09 22:08:57 +0000

    CI - First build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom"    // CHANGED
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"    // CHANGED
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import UpcomingMoviesPage from './pages/upcomingMoviesPage'
