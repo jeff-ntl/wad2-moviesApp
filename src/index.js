0e2e96d06e8e2eeb4e7a7c784a6fd07c51d72f29
Teek Leng Ng <jeff2406n@gmail.com>
2020-12-15 23:30:14 +0000

    implement route-based code splitting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import HomePage from "./pages/homePage";
-import MoviePage from './pages/movieDetailsPage'
+
 
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"    // CHANGED
-import TrendingMoviesPage from "./pages/trendingMoviesPage"
-import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
-import MovieReviewPage from "./pages/movieReviewPage";
-import UpcomingMoviesPage from './pages/upcomingMoviesPage'
-import AddMovieReviewPage from './pages/addMovieReviewPage'
+
 import SiteHeader from './components/siteHeader'
 
 import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
 import { AuthContext, AuthProvider } from "./Firebase/context";
-import SignIn from "./components/signIn";
 
+const HomePage = lazy (() => import("./pages/homePage"));
+const MoviePage = lazy (() => import("./pages/movieDetailsPage"));
+const TrendingMoviesPage = lazy (() => import("./pages/trendingMoviesPage"));
+const FavoriteMoviesPage = lazy (() => import("./pages/favoritesMoviesPage"));
+const MovieReviewPage = lazy (() => import("./pages/movieReviewPage"));
+const UpcomingMoviesPage = lazy (() => import("./pages/upcomingMoviesPage"));
+const AddMovieReviewPage = lazy (() => import("./pages/addMovieReviewPage"));
+const SignIn = lazy (() => import("./components/signIn"));
+/*
+import HomePage from "./pages/homePage";
+import MoviePage from './pages/movieDetailsPage'
+import TrendingMoviesPage from "./pages/trendingMoviesPage"
+import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
+import UpcomingMoviesPage from './pages/upcomingMoviesPage'
+import AddMovieReviewPage from './pages/addMovieReviewPage'
+import SignIn from "./components/signIn";
+*/
 const App = () => {
   //private route
   const PrivateRoute = ({ component: Component, ...rest }) => {
@@ -44,17 +56,19 @@ const App = () => {
         <div className="container-fluid">
           <MoviesContextProvider>
             <GenresContextProvider>
-              <Switch>
-                <Route path="/signin" exact component={SignIn} />
-                <Route exact path="/movies/trending" component={TrendingMoviesPage} />
-                <PrivateRoute exact path="/movies/favorites" component={FavoriteMoviesPage} />
-                <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
-                <Route exact path="/reviews/form" component={AddMovieReviewPage} />
-                <Route path="/reviews/:id" component={MovieReviewPage} />
-                <Route path="/movies/:id" component={(props) => <MoviePage {...props} key={props.match.params.id}/>} />     
-                <Route path="/" component={HomePage} />
-                <Redirect from="*" to="/" />
-              </Switch>
+              <Suspense fallback={<h1>Loading page....</h1>}>
+                <Switch>
+                  <Route path="/signin" exact component={SignIn} />
+                  <Route exact path="/movies/trending" component={TrendingMoviesPage} />
+                  <PrivateRoute exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                  <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
+                  <Route exact path="/reviews/form" component={AddMovieReviewPage} />
+                  <Route path="/reviews/:id" component={MovieReviewPage} />
+                  <Route path="/movies/:id" component={(props) => <MoviePage {...props} key={props.match.params.id}/>} />     
+                  <Route path="/" component={HomePage} />
+                  <Redirect from="*" to="/" />
+                </Switch>
+              </Suspense>
             </GenresContextProvider>
           </MoviesContextProvider>
         </div>
