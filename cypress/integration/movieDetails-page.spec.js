2a309a235069af7f0caef9dad34ea12851abb6d4
Teek Leng Ng <jeff2406n@gmail.com>
2020-12-15 21:21:11 +0000

    implement e2e testing on recommeneded movies and casts

--- cypress/integration/movieDetails-page.spec.js
+++ cypress/integration/movieDetails-page.spec.js
@@ -1,6 +1,9 @@
 let movieId = null
 let movie;
 let reviews;
+let recommendedMovies;
+let casts;
+
 describe("Movie Details Page", () => {
   before(() => {
     cy.request(
@@ -26,6 +29,22 @@ describe("Movie Details Page", () => {
         movie = movieDetails;
         return movieDetails.id;
       })
+      .then((arbitraryMovieIdignored) => {
+        movieId = arbitraryMovieIdignored
+        cy.request(`https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&page=1`)
+        .its("body")
+        .then((response) => {
+          recommendedMovies = response.results;
+        })
+      })
+      .then(()=> {
+        cy.request(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${Cypress.env("TMDB_KEY")}&language=en-US`)
+        .its("body")
+        .then((response) => {
+          casts = response.cast;
+        })
+      })
+      
   });
   beforeEach(() => {
     cy.visit(`/`);
@@ -34,6 +53,11 @@ describe("Movie Details Page", () => {
 
   it("should display movie title in the page header", () => {
     cy.get("h2").contains(movie.title);
+  });
+  it("should display the movie poster", () => {
+    cy.get("img")
+      .should("have.attr", "src")
+      .should("include", movie.poster_path);
   });
   it("should display the movie's details", () => {
     cy.get("h4").contains("Overview");
@@ -47,15 +71,28 @@ describe("Movie Details Page", () => {
         cy.get("li").eq(3).contains(movie.release_date);
       });
   });
+  it("should display the recommended movies' details", () => {
+    cy.get("h4").contains("You might also like:");
+    cy.get("div.card").find("img.center").eq(0)
+      .should("have.attr", "src")
+      .should("include", recommendedMovies[0].poster_path);
+    cy.get("div.col-8").find("h4").eq(0).contains(recommendedMovies[0].title);
+  });
+  it("should display movie casts", () => {
+    cy.get("div.cast").find("h2").contains("Cast:");
+    cy.get("div.cast").find("img").eq(0)
+      .should("have.attr", "src")
+      .should("include", casts[0].profile_path );
+  });
+  it("should allow navigation to recommended movie", () => {
+    cy.get("div.col-8").find("h4").eq(0).click()
+    cy.url().should("include", recommendedMovies[0].id);
+    cy.get("h4").next().contains(recommendedMovies[0].overview);
+  });
   it("should display the Home icon with the correct URL value", () => {
     cy.get(".fa-home")
       .parent()
       .should("have.attr", "href")
       .should("include", movie.homepage);
-  });
-  it("should display the movie poster", () => {
-    cy.get("img")
-      .should("have.attr", "src")
-      .should("include", movie.poster_path);
   });
 });
